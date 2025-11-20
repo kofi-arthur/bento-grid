ff5db24189faa1a87516cf5ea16fb3abb4fc863f
kofi-arthur <paularthurjnr611@gmail.com>
2025-11-20 23:28:14 +0000

    Add comment component structure
    Implements the visual structure and basic functionality for a comment component.
    
    This includes:
    - Displaying user profile information (avatar, name, email, timestamp).
    - Adding action buttons for sending and deleting.
    - Rendering the comment body content.
    - Incorporating an input field for replies and an emoji selection area.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,54 @@
 import styles from './styles/app.module.css'
 
+import user from './assets/user.png'
+
 export default function App() {
     return (
         <main className={styles.bentoLayout}>
             <section className={styles.bentoBox}>
-                <div className={styles.comment}></div>
+                <div className={styles.comment}>
+                    <div className={styles.commentHeader}>
+                        <div className={styles.profileInfo}>
+                            <div className={styles.profile}>
+                                <img src={user} alt="" />
+                            </div>
+
+                            <div className={styles.userInfo}>
+                                <h3>Themanwitha Mask</h3>
+                                <a href="mailto:tmwam@example.com">tmwam@example.com</a>
+                            </div>
+
+                            <span className={styles.lastSeen}>Yesterday, 10:52 am</span>
+                        </div>
+
+                        <div className={styles.actions}>
+                            <button><i className="fal fa-send"></i></button>
+                            <button><i className="fal fa-trash-can"></i></button>
+                        </div>
+                    </div>
+
+                    <div className={styles.commentBody}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quaerat architecto dolores itaque neque accusantium. Ullam magni provident ex eaque ea mollitia id sed reiciendis laborum sint itaque quo deserunt, omnis modi beatae quod a</p>
+                    </div>
+
+                    <div className={styles.commentAction}>
+                        <input type="text" placeholder='Type here...' />
+
+                        <div className={styles.emojiContainer}>
+                            <div className={styles.emojiList}>
+                                <span className={styles.emoji}>😂</span>
+                                <span className={styles.emoji}>❤️‍🔥</span>
+                                <span className={styles.emoji}>🔥</span>
+                                <span className={styles.emoji}>⚡</span>
+                                <span className={styles.emoji}>👍🏽</span>
+                            </div>
+
+                            <button className={styles.emojiBtn}>
+                                <i className="far fa-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
                 <div className={styles.todo}></div>
             </section>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import './styles/all.css'
 import './styles/index.css'
+import './styles/fonts.css'
+import './styles/variables.css'
+
+import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
